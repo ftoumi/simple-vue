cf1c85e4bd50580439697ed89e563b32acfd680f
ftoumi <ftoumi@gmail.com>
2020-10-27 16:06:42 +0100

    new component

---
+++ src/components/PlayerCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <h3>{{ player.name }} ({{ player.number }})</h3>
+  <p>{{ player.position }}</p>
+</template>
+
+<script>
+export default {
+  name: "player-card",
+  props: ['player']
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/PlayerList.vue
@@ -0,0 +1,35 @@
+<template>
+  <h1>Liste des joueurs {{ playersData.name }} </h1>
+
+  <div>Sélection XX joueurs</div>
+
+  <player-card
+    v-for="(player, index) in playersData.players"
+    :key="index"
+    :player="player"
+    >
+  </player-card>
+
+</template>
+
+<script>
+
+import playersData from '../assets/data/players.json'
+import PlayerCard from './PlayerCard.vue'
+
+export default {
+  name: "player-List",
+  components: {
+    PlayerCard
+  },
+  data() {
+    return {
+      playersData : playersData
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
