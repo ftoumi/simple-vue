c5752c06f65019e3066a88441ff85972e39ace51
ftoumi <ftoumi@gmail.com>
2020-10-27 16:46:05 +0100

    tailwind

--- src/components/PlayerCard.vue
+++ src/components/PlayerCard.vue
@@ -1,15 +1,26 @@
 <template>
-  <h3>{{ player.name }} ({{ player.number }})</h3>
-  <p>{{ player.position }}</p>
+  <div class="border border-green-900 m-2 p-2 rounded bg-gray-300">
+    <h3>{{ playerName }} ({{ player.number }})</h3>
+    <p>{{ player.position }}</p>
+  </div>
+  
 </template>
 
 <script>
 export default {
   name: "player-card",
-  props: ['player']
+  props: ['player'],
+  computed: {
+    playerName() {
+      if(this.player.name.split('.').length > 1){
+        return this.player.name.split('.')[1]
+      }
+      return this.player.name;
+    }
+  }
 }
 </script>
 
-<style>
+<style scoped>
 
 </style>
